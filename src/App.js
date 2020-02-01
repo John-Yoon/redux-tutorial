b555ff942f600accfdf2d070535af9a8c4656769
John-Yoon <jhyoon186@gmail.com>
2020-02-01 17:11:04 +0900

    Redux  1) 컴퍼넌트화 하기위해 분리시킴.  2) subscribe & dispatch 구현.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux";
+import {updateUser} from "./redux/actions";
 
-function App() {
+function App(props) {
+  console.log(props);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>product length: {props.product.length}</p>
+      <p>{props.user}</p>
+        <button onClick={() => props.updateUser('Jane')}>Action Test</button>
     </div>
   );
 }
 
-export default App;
+// subscribe: Store의 state를 props 매핑한다.
+const mapStateToProps = (state) => ({
+  // 왼쪽 props, 오른쪽 store.state
+  product: state.productReducer,
+  user: state.userReducer
+});
+
+//dispatch: (Action을 dispatch하는) function을 props로 매핑.
+const mapActionToProps = (dispatch) => ({
+    // L - props, R - function
+    updateUser: (name) => dispatch(updateUser(name))
+})
+
+// export default App;
+// HoC, 커링 function
+export default connect(mapStateToProps, mapActionToProps)(App);
+
+// react-redux 7.0부터 redux hooks가 제공된다???
